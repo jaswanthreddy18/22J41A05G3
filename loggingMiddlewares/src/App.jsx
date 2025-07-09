bf1ca6265a5458c2ddb4421f389a636c45465761
Jaswanth Reddy <jaswanthreddyjas@gmail.com>
2025-07-09 12:23:44 +0530

    modified

--- loggingMiddlewares/src/App.jsx
+++ loggingMiddlewares/src/App.jsx
@@ -1,13 +1,6 @@
 import axios from 'axios'
 import './App.css'
 function App() {
-//   data: 
-// access_token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJqYXN3YW50aHJlZGR5amFzQGdtYWlsLmNvbSIsImV4cCI6MTc1MjAzOTA1NywiaWF0IjoxNzUyMDM4MTU3LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiOWU3Y2QxN2EtOThjOC00OTYwLTg0NTQtYThkZTgwY2MzODZiIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoia2FuZGFkaSBqYXN3YW50aCByZWRkeSIsInN1YiI6ImQ4ODhiZjc3LTgxMzEtNGFiYi04NTg2LTJmOGM5Y2NkYzM5MSJ9LCJlbWFpbCI6Imphc3dhbnRocmVkZHlqYXNAZ21haWwuY29tIiwibmFtZSI6ImthbmRhZGkgamFzd2FudGggcmVkZHkiLCJyb2xsTm8iOiIyMmo0MWEwNWczIiwiYWNjZXNzQ29kZSI6ImVlV0VyeCIsImNsaWVudElEIjoiZDg4OGJmNzctODEzMS00YWJiLTg1ODYtMmY4YzljY2RjMzkxIiwiY2xpZW50U2VjcmV0IjoiTmNXTWtyY1FCUnVodk1haCJ9.Xc9QPeYsHmjytDth2XGSZ7lKhYGEHHonkCFP4xNppoM"
-// expires_in :  1752039057
-// token_type : "Bearer"
-
-// logID  :  "11b3ecb1-d7ea-4eda-a25d-9eb994b1208d"
-// message : "log created successfully"
   const userData = {
     email : "jaswanthreddyjas@gmail.com",
     name : "Kandadi Jaswanth Reddy",
